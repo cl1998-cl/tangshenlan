567a213294567595a9bcc6a3f9506b652a0748e2
chenlong2529 <cl2529@hanweb.com>
2022-09-16 09:27:00 +0800

    首页翻转

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,22 +2,24 @@
     <div class="container">
         <!--       <s-z></s-z>-->
         <template v-for="item in cardList">
-            <div :ref="item.ref" class="card flex_item">
+            <div :id="item.ref" class="card flex_item" @mouseenter="cardEnter">
                 <div class="content_img">
                     <el-image fit="fit"
                               src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg"
                               style="height: 100%; width: 100%;"></el-image>
                 </div>
                 <div class="content">
-                    <h1>{{ item.title }}</h1>
-                    <h3>{{ item.sec }}</h3>
-                    <p> {{ item.text }}</p>
+                    <h1>{{ formatData(item, 'title') }}</h1>
+                    <h3>{{ formatData(item, 'sec') }}</h3>
+                    <p> {{ formatData(item, 'text') }}</p>
                     <div class="foot" @click.stop="addAnimate(item)">
-                        <i class="el-icon-cloudy"></i>
+                        <i v-show="item.show" class="el-icon-sunny"></i>
+                        <i v-show="!item.show" class="el-icon-moon"></i>
                     </div>
                 </div>
             </div>
         </template>
+        <div ref="test">{{test}}</div>
 
     </div>
 </template>
@@ -35,37 +37,105 @@ export default {
             formData: {},
             cardList: [
                 {
-                    title: 'No1',
-                    sec: 'swiper',
-                    text: '基于swiper实现的轮播图',
-                    ref: 'oneRef'
+                    list: [
+                        {
+                            title: 'No1正面',
+                            sec: 'search table',
+                            text: '喷喷喷',
+
+                        },
+                        {
+                            title: 'No1反面',
+                            sec: 'search table',
+                            text: '大大大',
+                            ref: 'oneRef'
+                        }
+                    ],
+                    ref: 'oneRef',
+                    show: true
                 },
                 {
-                    ref: 'twoRef'
+                    ref: 'twoRef',
+                    list: [],
+                    show: true
                 },
                 {
-                    ref: 'threeRef'
+                    ref: 'threeRef',
+                    list: [],
+                    show: true
                 },
                 {
-                    ref: 'fourRef'
+                    ref: 'fourRef',
+                    list: [],
+                    show: true
                 },
                 {
-                    ref: 'fiveRef'
+                    ref: 'fiveRef',
+                    list: [],
+                    show: true
                 },
                 {
-                    ref: 'sixRef'
+                    ref: 'sixRef',
+                    list: [],
+                    show: true
                 }
-            ]
+            ],
+            test: '123'
         }
     },
     async mounted () {
 
     },
     methods: {
-        addAnimate({ref}){
-            let dom = this.$refs[ref]
-            console.log(dom)
-        }
+        addAnimate (item) {
+            this.test = Math.random()
+            console.log('test', this.$refs.test.innerHTML)
+            Promise.resolve().then(res => {
+                console.log('wei', this.$refs.test.innerHTML)
+            })
+            this.$nextTick(() => {
+                console.log('next', this.$refs.test.innerHTML)
+            })
+            let dom = document.getElementById(item.ref)
+            let children = [...dom.childNodes]
+            children.forEach(node => {
+                node.style.opacity = 0
+                this.animate('tran', node)
+                setTimeout(() => {
+                    node.style.opacity = 1
+                }, 1000)
+            })
+            this.animate('tran', dom)
+            setTimeout(() => {
+                item.show = !item.show
+            }, 1000)
+        },
+        animate (na, dom) {
+            let classes = dom.className
+            if (classes.includes(na)) {
+                dom.className = dom.className.replace(na, '')
+                dom.style.backgroundColor = 'rgba(255,255,255,.85)'
+            } else {
+                dom.className = na + ' ' + classes
+                dom.style.backgroundColor = 'rgba(255,255,255,.1)'
+            }
+        },
+        formatData(item, t){
+            let num = item.show ? 0 : 1
+            let obj = item.list[num]
+            if(obj) return obj[t]
+            return ""
+        },
+        cardEnter(e){
+            console.log(e)
+            let a
+            a = 1
+            a = 2
+            a = 3
+            //a = 3
+        },
+
+
     }
 }
 </script>
@@ -99,7 +169,7 @@ export default {
     height: 300px;
     box-shadow: 20px 20px 50px rgba(0, 0, 0, .5);
     border-radius: 15px;
-    background: rgba(255, 255, 255, 0.1);
+    background: rgba(255, 255, 255, 0.85);
     overflow: hidden;
     border-top: 1px solid rgba(255, 255, 255, 0.5);
     border-left: 1px solid rgba(255, 255, 255, 0.5);
@@ -162,5 +232,13 @@ export default {
     rignt: 0;
     right: 30px;
     cursor pointer
+}
+
+.tran {
+    transform rotateX(180deg)
+}
+
+.flex_item {
+    transition 2s
 }
 </style>
